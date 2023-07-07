ebdd3823478d4d76f22c9e0f085209720d509251
Squiddy24 <130530087+Squiddy24@users.noreply.github.com>
2023-07-07 16:51:42 -0400

    Added blackout effect

---
+++ GMTK Game Jam 2023/Assets/UnlockScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockScript : MonoBehaviour
+{
+    public int CurrentLevel;
+    public int LevelThreshold;
+    public SpriteRenderer spriteRender;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRender.color = new Color32(0, 0, 0, 255);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CurrentLevel >= LevelThreshold)
+        {
+            spriteRender.color = new Color32(255, 255, 255, 255);
+        }
+        else
+        {
+            spriteRender.color = new Color32(0, 0, 0, 255);
+        }
+    }
+}
